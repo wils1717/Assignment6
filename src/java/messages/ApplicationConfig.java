23253a739f113d177a03ae95efe79f70cf8a329f
wils1717 <mark_wilson17@live.ca>
2017-03-22 16:16:59 -0400

    finished

--- src/java/messages/ApplicationConfig.java
+++ src/java/messages/ApplicationConfig.java
@@ -1,19 +1,14 @@
 /*
- * Copyright 2016 Len Payne <len.payne@lambtoncollege.ca>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package messages;
+
+/**
+ *
+ * @author c0533886
+ */
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
@@ -29,12 +24,6 @@ public class ApplicationConfig extends Application {
         return resources;
     }
 
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(messages.MessageService.class);
     }
