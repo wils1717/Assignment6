f93910679211e35718fc258e958cc2431bd69881
wils1717 <mark_wilson17@live.ca>
2017-03-29 11:35:55 -0400

    fix db

--- src/java/messages/DBUtils.java
+++ src/java/messages/DBUtils.java
@@ -10,25 +10,39 @@ package messages;
  * @author c0533886
  */
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.enterprise.context.ApplicationScoped;
 
-
-@ApplicationScoped
+/**
+ * A Database Utility Class
+ *
+ * @author <Mark Wilson>
+ */
 public class DBUtils {
 
+    private final static String studentNumber = "c0533886";
+
+    /**
+     * Utility method used to create a Database Connection
+     *
+     * @return the Connection object
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String jdbc = "jdbc:mysql://ipro:3306/messagedb";
-        String user = "messagedb";
-        String pass = "password";
-        return DriverManager.getConnection(jdbc, user, pass);
+        String server = "ipro.lambton.on.ca";
+        String username = studentNumber + "-java";
+        String password = studentNumber;
+        String database = username;
+        String jdbc = String.format("jdbc:mysql://%s/%s", server, database);
+        return DriverManager.getConnection(jdbc, username, password);
     }
-
 }
+
